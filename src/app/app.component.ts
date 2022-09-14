758ba247c9587d7269618eef53658d1dec321864
Hiago Bernardi <hiago.bernardi@gmail.com>
2022-09-14 15:25:54 +0100

    Calculation finalized

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import { ValueType } from 'src/enums/value-type.enum';
 export class AppComponent implements OnInit {
   options: string[] = ['10', '13', '20'];
   form: FormGroup;
+  lastChanged: ValueType;
 
   constructor(
     private formBuilder: FormBuilder
@@ -27,31 +28,55 @@ export class AppComponent implements OnInit {
   }
 
   rateChange(e: MatSelectChange) {
-    
+    if(this.form.value.netPrice || this.form.value.grossPrice || this.form.value.amountsVAT)  
+    {
+      if(this.form.value.netPrice && this.lastChanged == ValueType.NetPrice)
+      {
+        this.valueChange(ValueType.NetPrice);
+        return;
+      }
+      if(this.form.value.grossPrice && this.lastChanged == ValueType.GrossPrice)
+      {
+        this.valueChange(ValueType.GrossPrice);
+        return;
+      }
+      if(this.form.value.amountsVAT && this.lastChanged == ValueType.amountsVAT)
+      {
+        this.valueChange(ValueType.amountsVAT);
+        return;
+      }
+    } 
 
   }
 
   valueChange(valueType: ValueType) {
+    this.lastChanged = valueType;
     if(this.form.value.rateVAT)
         {
           switch(valueType) {
             case ValueType.NetPrice:
               this.form.controls["amountsVAT"].setValue(
-                Number(this.form.value.netPrice) * (this.form.value.rateVAT / 100)
+                (Number(this.form.value.netPrice) * (this.form.value.rateVAT / 100)).toFixed(2)
               );
               this.form.controls["grossPrice"].setValue(
-                Number(this.form.value.netPrice) + this.form.value.amountsVAT
+                (Number(this.form.value.netPrice) + Number(this.form.value.amountsVAT)).toFixed(2)
               );              
               break;
             case ValueType.GrossPrice:
               this.form.controls["amountsVAT"].setValue(
-                (Number(this.form.value.grossPrice) * Number(this.form.value.rateVAT)) / (100 + Number(this.form.value.rateVAT))
+                ((Number(this.form.value.grossPrice) * Number(this.form.value.rateVAT)) / (100 + Number(this.form.value.rateVAT))).toFixed(2)
               );
               this.form.controls["netPrice"].setValue(
-                Number(this.form.value.grossPrice) - this.form.value.amountsVAT
+                (Number(this.form.value.grossPrice) - Number(this.form.value.amountsVAT)).toFixed(2)
               );
               break;
             case ValueType.amountsVAT:
+              this.form.controls["netPrice"].setValue(
+                ((Number(this.form.value.amountsVAT) * 100) / Number(this.form.value.rateVAT)).toFixed(2)
+              );
+              this.form.controls["grossPrice"].setValue(
+                (Number(this.form.value.netPrice) + Number(this.form.value.amountsVAT)).toFixed(2)
+              ); 
               break;
             default:
               break;
